fbdf462105922748fb48d8aac4cae07bda66ba87
Sam Ritchie <sritchie09@gmail.com>
2011-05-09 00:58:36 -0600

    finished ->> macros.

--- src/pallet/thread_expr.clj
+++ src/pallet/thread_expr.clj
@@ -3,11 +3,11 @@
   (:require [clojure.contrib.macro-utils :as macro]))
 
 (defn- for-
-  [arg seq-exprs body]
+  [threader arg seq-exprs body]
   `(reduce #(%2 %1)
              ~arg
              (for ~seq-exprs
-               (fn [arg#] (-> arg# ~@body)))))
+               (fn [arg#] (~threader arg# ~@body)))))
 
 (defmacro for->
   "Apply a thread expression to a sequence.
@@ -17,7 +17,7 @@
           (+ x)))
    => 7"
   [arg seq-exprs & body]
-  (for- arg seq-exprs body))
+  (for- 'clojure.core/-> arg seq-exprs body))
 
 (defmacro for->>
   "Apply a thread expression to a sequence.
@@ -29,13 +29,13 @@
   [seq-exprs & body]
   (let [arg (last body)
         body (butlast body)]
-    (for- arg seq-exprs body)))
+    (for- 'clojure.core/->> arg seq-exprs body)))
 
 (defn- when-
-  [arg condition body]
+  [threader arg condition body]
   `(let [arg# ~arg]
      (if ~condition
-       (-> arg# ~@body)
+       (~threader arg# ~@body)
        arg#)))
 
 (defmacro when->
@@ -46,7 +46,7 @@
           (+ 1)))
    => 2"
   [arg condition & body]
-  (when- arg condition body))
+  (when- 'clojure.core/-> arg condition body))
 
 (defmacro when->>
   "A `when` form that can appear in a request thread.
@@ -58,26 +58,13 @@
   [condition & body]
   (let [arg (last body)
         body (butlast body)]
-    (when- arg condition body)))
-
-(defmacro when-not->
-  "A `when-not` form that can appear in a request thread.
-   eg.
-      (-> 1
-        (when-not-> true
-          (+ 1)))
-   => 1"
-  [arg condition & body]
-  `(let [arg# ~arg]
-     (if-not ~condition
-       (-> arg# ~@body)
-       arg#)))
+    (when- 'clojure.core/->> arg condition body)))
 
 (defn- when-not-
-  [arg condition body]
+  [threader arg condition body]
   `(let [arg# ~arg]
      (if-not ~condition
-       (-> arg# ~@body)
+       (~threader arg# ~@body)
        arg#)))
 
 (defmacro when-not->
@@ -88,7 +75,7 @@
           (+ 1)))
    => 1"
   [arg condition & body]
-  (when-not- arg condition body))
+  (when-not- 'clojure.core/-> arg condition body))
 
 (defmacro when-not->>
   "A `when-not` form that can appear in a request thread.
@@ -100,12 +87,12 @@
   [condition & body]
   (let [arg (last body)
         body (butlast body)]
-    (when-not- arg condition body)))
+    (when-not- 'clojure.core/->> arg condition body)))
 
 (defn- let-
-  [arg binding body]
+  [threader arg binding body]
   `(let ~binding
-     (-> ~arg ~@body)))
+     (~threader ~arg ~@body)))
 
 (defmacro let->
   "A `let` form that can appear in a request thread.
@@ -115,7 +102,7 @@
           (+ a)))
    => 2"
   [arg binding & body]
-  (let- arg binding body))
+  (let- 'clojure.core/-> arg binding body))
 
 (defmacro let->>
   "A `let` form that can appear in a request thread.
@@ -127,7 +114,12 @@
   [binding & body]
   (let [arg (last body)
         body (butlast body)]
-    (let- arg binding body)))
+    (let- 'clojure.core/->> arg binding body)))
+
+(defn- binding-
+  [threader arg bindings body]
+  `(binding ~bindings
+     (~threader ~arg ~@body)))
 
 (defmacro binding->
   "A `binding` form that can appear in a request thread.
@@ -138,8 +130,27 @@
           (+ *a*)))
    => 2"
   [arg bindings & body]
-  `(binding ~bindings
-     (-> ~arg ~@body)))
+  (binding- 'clojure.core/-> arg bindings body))
+
+(defmacro binding->>
+  "A `binding` form that can appear in a request thread.
+   eg.
+      (def *a* 0)
+      (->> 1
+         (binding->> [*a* 1]
+           (+ *a*)))
+   => 2"
+  [bindings & body]
+  (let [arg (last body)
+        body (butlast body)]
+    (binding- 'clojure.core/->> arg bindings body)))
+
+(defn- when-let-
+  [threader arg binding body]
+  `(let [arg# ~arg]
+     (if-let ~binding
+       (~threader arg# ~@body)
+       arg#)))
 
 (defmacro when-let->
   "A `when-let` form that can appear in a request thread.
@@ -149,10 +160,31 @@
           (+ a)))
    => 2"
   [arg binding & body]
-  `(let [arg# ~arg]
-     (if-let ~binding
-       (-> arg# ~@body)
-       arg#)))
+  (when-let- 'clojure.core/-> arg binding body))
+
+(defmacro when-let->>
+  "A `when-let` form that can appear in a request thread.
+   eg.
+      (->> 1
+         (when-let->> [a 1]
+           (+ a)))
+   => 2"
+  [binding & body]
+  (let [arg (last body)
+        body (butlast body)]
+    (when-let- 'clojure.core/->> arg binding body)))
+
+(defn- if-
+  ([threader arg condition form]
+     `(let [arg# ~arg]
+        (if ~condition
+          (~threader arg# ~form)
+          arg#)))
+  ([threader arg condition form else-form]
+     `(let [arg# ~arg]
+        (if ~condition
+          (~threader arg# ~form)
+          (~threader arg# ~else-form)))))
 
 (defmacro if->
   "An `if` form that can appear in a request thread
@@ -163,15 +195,35 @@
           (+ 2)))
    => 2"
   ([arg condition form]
-     `(let [arg# ~arg]
-        (if ~condition
-          (-> arg# ~form)
-          arg#)))
+     (if- 'clojure.core/-> arg condition form))
   ([arg condition form else-form]
+     (if- 'clojure.core/-> arg condition form else-form)))
+
+(defmacro if->>
+  "An `if` form that can appear in a request thread
+   eg.
+      (->> 1
+         (if->> true
+           (+ 1)
+           (+ 2)))
+   => 2"
+  ([condition form arg]
+     (if- 'clojure.core/->> arg condition form))
+  ([condition form else-form arg]
+     (if- 'clojure.core/->> arg condition form else-form)))
+
+(defn- if-not-
+  ([threader arg condition form]
      `(let [arg# ~arg]
-        (if ~condition
-          (-> arg# ~form)
-          (-> arg# ~else-form)))))
+        (if-not ~condition
+          (~threader arg# ~form)
+          arg#)))
+  ([threader arg condition form else-form]
+     `(let [arg# ~arg]
+        (if-not ~condition
+          (~threader arg# ~form)
+          (~threader arg# ~else-form)))))
+
 
 (defmacro if-not->
   "An `if-not` form that can appear in a request thread
@@ -182,14 +234,27 @@
           (+ 2)))
    => 3"
   ([arg condition form]
-     `(let [arg# ~arg]
-        (if-not ~condition
-          (-> arg# ~form))))
+     (if-not- 'clojure.core/-> arg condition form))
   ([arg condition form else-form]
-     `(let [arg# ~arg]
-        (if-not ~condition
-          (-> arg# ~form)
-          (-> arg# ~else-form)))))
+     (if-not- 'clojure.core/-> arg condition form else-form)))
+
+(defmacro if-not->>
+  "An `if-not` form that can appear in a request thread
+   eg.
+      (->> 1
+         (if-not->> true
+           (+ 1)
+           (+ 2)))
+   => 3"
+  ([condition form arg]
+     (if-not- 'clojure.core/->> arg condition form))
+  ([condition form else-form arg]
+     (if-not- 'clojure.core/->> arg condition form else-form)))
+
+(defn- arg-
+  [threader arg sym body]
+  `(let [~sym ~arg]
+     (~threader ~sym ~@body)))
 
 (defmacro arg->
   "Lexically assign the threaded argument to the specified symbol.
@@ -199,8 +264,25 @@
 
        => 2"
   [arg [sym] & body]
-  `(let [~sym ~arg]
-     (-> ~sym ~@body)))
+  (arg- 'clojure.core/-> arg sym body))
+
+(defmacro arg->>
+  "Lexically assign the threaded argument to the specified symbol.
+
+       (->> 1
+          (arg->> [x] (+ x)))
+
+       => 2"
+  [[sym] & body]
+  (let [arg (last body)
+        body (butlast body)]
+    (arg- 'clojure.core/->> arg sym body)))
+
+(defn- let-with-arg-
+  [threader arg arg-symbol binding body]
+  `(let [~arg-symbol ~arg]
+     (let ~binding
+       (~threader ~arg-symbol ~@body))))
 
 (defmacro let-with-arg->
   "A `let` form that can appear in a request thread, and assign the
@@ -212,9 +294,26 @@
           (+ a val)))
    => 3"
   [arg arg-symbol binding & body]
-  `(let [~arg-symbol ~arg]
-     (let ~binding
-       (-> ~arg-symbol ~@body))))
+  (let-with-arg- 'clojure.core/-> arg arg-symbol binding body))
+
+(defmacro let-with-arg->>
+  "A `let` form that can appear in a request thread, and assign the
+   value of the threaded arg.
+
+   eg.
+      (->> 1
+         (let-with-arg->> val [a 1]
+           (+ a val)))
+   => 3"
+  [arg-symbol binding & body]
+  (let [arg (last body)
+        body (butlast body)]
+    (let-with-arg- 'clojure.core/->> arg arg-symbol binding body)))
+
+(defn- apply-
+  [arg f arg-coll]
+  `(let [arg# ~arg]
+     (apply ~f arg# ~@arg-coll)))
 
 (defmacro apply->
   "Apply in a threaded expression.
@@ -222,9 +321,24 @@
       (-> 1
         (apply-> + [1 2 3]))
    => 7"
-  [request f & args]
-  `(let [request# ~request]
-     (apply ~f request# ~@args)))
+  [arg f & arg-coll]
+  (apply- arg f arg-coll))
+
+(defmacro apply->>
+  "Apply in a threaded expression.
+   e.g.
+      (->> 1
+         (apply->> + [1 2 3]))
+   => 7"
+  [f & arg-coll]
+  (let [arg (last arg-coll)
+        arg-coll (butlast arg-coll)]
+    (apply- arg f arg-coll)))
+
+(defn apply-map-
+  [arg f arg-coll]
+  `(let [arg# ~arg]
+     (apply ~f arg# ~@(butlast arg-coll) (apply concat ~(last arg-coll)))))
 
 (defmacro apply-map->
   "Apply in a threaded expression.
@@ -232,9 +346,19 @@
       (-> :a
         (apply-map-> hash-map 1 {:b 2}))
    => {:a 1 :b 2}"
-  [request f & args]
-  `(let [request# ~request]
-     (apply ~f request# ~@(butlast args) (apply concat ~(last args)))))
+  [arg f & arg-coll]
+  (apply-map- arg f arg-coll))
+
+(defmacro apply-map->
+  "Apply in a threaded expression.
+   e.g.
+      (-> :a
+        (apply-map-> hash-map 1 {:b 2}))
+   => {:a 1 :b 2}"
+  [f & arg-coll]
+  (let [arg (last arg-coll)
+        arg-coll (butlast arg-coll)]
+    (apply-map- arg f arg-coll)))
 
 (defmacro -->
   "Similar to `clojure.core/->`, with added symbol macros for the
@@ -275,3 +399,43 @@
      ~'apply apply->
      ~'expose-request-as arg->]
     (-> ~@forms)))
+
+(defmacro -->>
+  "Similar to `clojure.core/->>`, with added symbol macros for the
+  various threading macros found in `pallet.thread-expr`. Currently
+  supported symbol macros are:
+
+* binding
+* for
+* let
+* when, when-not, when-let
+* if, if-not
+* apply
+* expose-request-as (bound to `pallet.thread-expr/arg->>`
+
+    Examples:
+
+     (-->> 5
+         (let [y 1]
+           (for [x (range 3)]
+             (+ x y)))
+         (+ 1))
+ => 12
+    
+      (-->> 5
+            (expose-request-as [x] (+ x))
+            (+ 1))
+ => 11"
+  [& forms]
+  `(macro/symbol-macrolet
+    [~'binding binding->>
+     ~'for for->>
+     ~'if if->>
+     ~'if-not if-not->>
+     ~'when when->>
+     ~'when-not when-not->>
+     ~'when-let when-let->>
+     ~'let let->>
+     ~'apply apply->>
+     ~'expose-request-as arg->>]
+    (->> ~@forms)))
